e32ec4fb3315e4ad97fe7b50f9d0c42f34ce2556
AMIT <86924251+amitxv@users.noreply.github.com>
2024-02-10 20:53:20 +0000

    Use rootcopy instead of modules and separate linpack setup to setup_linpack.sh

--- build.sh
+++ build.sh
@@ -33,7 +33,7 @@ main() {
     wget -O "$file_name" "$src/$file_name"
 
     if [ ! -e "./$file_name" ]; then
-        echo "error: download failed"
+        echo "error: $file_name download failed"
         return 1
     fi
 
@@ -62,28 +62,19 @@ main() {
     rm "./extracted_iso/porteus/base/002-xtra.xzm"
     rm "./extracted_iso/porteus/base/003-openbox.xzm"
 
-    # setup linpack
-    wget -O "linpack.tgz" "https://downloadmirror.intel.com/793598/l_onemklbench_p_2024.0.0_49515.tgz"
-    # extract
-    7z x ./linpack.tgz
-    7z x ./linpack.tar -o"./linpack"
+    # Setup Linpack
+    linpack_binary="./porteus/porteus/rootcopy/usr/bin"
 
-    # version name changes in folder name
-    benchmarks_folder=$(find "./linpack" -maxdepth 1 -type d -name "*benchmarks*" -print -quit)
-
-    # create linpack/usr/bin and copy official binary there as module will be packed later
-    mkdir -p "./porteus/porteus/modules/linpack/usr/bin"
-    cp "$benchmarks_folder/linux/share/mkl/benchmarks/linpack/xlinpack_xeon64" "./porteus/porteus/modules/linpack/usr/bin"
-
-    # patch binary for AMD
-    if ! python3 "./patch_linpack.py" "./porteus/porteus/modules/linpack/usr/bin/xlinpack_xeon64"; then
-        echo "error: failed to patch binary"
+    if ! bash "./setup_linpack.sh" $linpack_binary; then
+        echo "error: failed to setup linpack"
         return 1
     fi
 
-    # create linpack module
-    mksquashfs "./porteus/porteus/modules/linpack" "./porteus/porteus/modules/linpack.xzm"
-    rm -r "./porteus/porteus/modules/linpack"
+    # patch linpack binary for AMD
+    if ! python3 "./patch_linpack.py" "$linpack_binary/xlinpack_xeon64"; then
+        echo "error: failed to patch binary"
+        return 1
+    fi
 
     # move custom files
     cp -ru ./porteus/* "./extracted_iso"
