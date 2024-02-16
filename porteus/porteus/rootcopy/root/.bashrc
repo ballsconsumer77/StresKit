bab603ea41fc82efd61f9ab906d6ba54279cedce
AMIT <86924251+amitxv@users.noreply.github.com>
2024-02-16 17:42:32 +0000

    Create linpack interface

--- porteus/porteus/rootcopy/root/.bashrc
+++ porteus/porteus/rootcopy/root/.bashrc
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 alias sensors='watch -n 1 sensors coretemp-isa-0000'
-alias linpack='(cd /root/linpack && ./runme_xeon64.sh)'
+alias linpack='function __linpack() { (cd /root/linpack && ./runme_xeon64.sh "$@"); unset -f __linpack; }; __linpack'
 alias prime95='(cd /root/prime95 && ./mprime)'
 alias ycruncher='(cd /root/ycruncher && ./y-cruncher)'
--- porteus/porteus/rootcopy/root/linpack/runme_xeon64.sh
+++ porteus/porteus/rootcopy/root/linpack/runme_xeon64.sh
@@ -1,9 +1,95 @@
 #!/bin/sh
 
-export KMP_AFFINITY=nowarnings,compact,1,0,granularity=fine
+usage() {
+    echo "Usage: $0 [-m <gb>] [-s <samples>]"
+}
 
-# add optional argument for samples and memory size in GB
-# use 80% of memory and 100 samples by default
-# generate lininput_xeon64 (remove from dir as well)
+is_psize_valid() {
+    psize=$1
+    has_avx=$2
 
-./xlinpack_xeon64 lininput_xeon64
+    if [ "$has_avx" -eq 1 ]; then
+        if ((psize % 16 == 0)) && (( (psize / 16) % 2 == 1 )) && (( psize % 32 != 0 )); then
+            return 0
+        fi
+    else
+        if ((psize % 8 == 0)) && (( (psize / 8) % 2 == 1 )) && (( psize % 16 != 0 )); then
+            return 0
+        fi
+    fi
+
+    return 1
+}
+
+main() {
+    memory_arg=""
+    samples_arg=""
+
+    while getopts "m:s:" opt; do
+        case $opt in
+            m)
+                memory_arg="$OPTARG"
+                ;;
+            s)
+                samples_arg="$OPTARG"
+                ;;
+            \?)
+                echo "error: invalid option: -$OPTARG"
+                usage
+                exit 1
+                ;;
+            :)
+                echo "error: option -$OPTARG requires an argument"
+                usage
+                exit 1
+                ;;
+        esac
+    done
+
+    if [ -n "$memory_arg" ]; then
+        memory_size=$memory_arg
+    else
+        # use 80% of available memory by default
+        mem_total_gb=$(echo "$(grep MemTotal /proc/meminfo | awk '{print $2}') / 1024^2" | bc)
+        memory_size=$(echo "scale=2; $mem_total_gb * 0.8" | bc)
+    fi
+
+    samples=${samples_arg:-100}
+
+    has_avx=$(grep -q "avx" /proc/cpuinfo && echo 1 || echo 0)
+    memory_in_bytes=$(echo "$memory_size * 1073741824" | bc)
+    initial_psize=$(echo "sqrt($memory_in_bytes / 8)" | bc)
+    optimal_psize=0
+    n=0
+
+    if is_psize_valid "$initial_psize" "$has_avx"; then
+    optimal_psize=$initial_psize
+    fi
+
+    while [ "$optimal_psize" -eq 0 ]; do
+        temp_problem_size=$((initial_psize + n))
+        if is_psize_valid $temp_problem_size "$has_avx"; then
+            optimal_psize=$temp_problem_size
+        fi
+
+        temp_problem_size=$((initial_psize - n))
+        if is_psize_valid $temp_problem_size "$has_avx"; then
+            optimal_psize=$temp_problem_size
+        fi
+
+        n=$((n + 1))
+    done
+
+    # create lininput
+    echo -e "\n\n1\n$optimal_psize\n$optimal_psize\n$samples\n4" > "lininput_xeon64"
+
+    # environment variables for linpack
+    export KMP_AFFINITY=nowarnings,compact,1,0,granularity=fine
+
+    # run linpack
+    ./xlinpack_xeon64 lininput_xeon64
+    return 0
+}
+
+
+main "$@"
